fbbc5109421744b7ce4165c58c97589b230b2d11
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-08-05 16:46:11 +0600

    owl carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./containers/HomePage/index";
+import "./App.css";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { getAllCategory, isUserLoggedIn, updateCart,getAddress } from "./store/actions";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ProductListPage from './containers/ProductListPage/index'
+import ProductDetailsPage from './containers/ProductDetailsPage'
+import CartPage from './containers/CartPage/index'
+import CheckoutPage from './containers/CheckoutPage/index'
+import OrderDetailsPage from './containers/OrderDetailsPage/index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import OrderPage from "./containers/OrderPage";
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getAllCategory: () => dispatch(getAllCategory()),
+    isUserLoggedIn: () => dispatch(isUserLoggedIn()),
+    updateCart : ()=>dispatch(updateCart()),
+    getAddress:()=>dispatch(getAddress())
+
+  };
+};
+
+
+class App extends Component {
+  componentDidMount() {
+    this.props.getAllCategory();
+    this.props.isUserLoggedIn();
+    this.props.updateCart();
+    this.props.getAddress()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/account/orders"  component={OrderPage} />
+            <Route exact path="/checkout"  component={CheckoutPage} />
+            <Route exact path="/cart"  component={CartPage} />
+            <Route exact path="/"  component={HomePage} />
+            <Route path="/order_details/:orderId" component={OrderDetailsPage} />
+            <Route path="/:productSlug/:productId/p" component={ProductDetailsPage} />
+            <Route path="/:slug" component={ProductListPage} />
+
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default connect(null, mapDispatchToProps)(App);
